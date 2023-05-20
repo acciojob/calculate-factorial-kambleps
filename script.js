4f2da6616083a7fc0a5522888fd87eaa6e8277b4
kambleps <125243027+kambleps@users.noreply.github.com>
2023-05-20 18:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let num = prompt("Enter the number");
+let count=1;
+for(let i=1;i<=num;i++)
+	{
+		count =count*i;
+	}
+alert("The factorial of "+num+" is "+count);
